6d5ce8f6dabd8cdf138b9c98c20b9b7469e2d909
Fred <fredzhangziji@gmail.com>
2021-01-26 16:54:45 -0500

    update

---
+++ Binary Search/LintCode 14. First Postion of Target.py
@@ -0,0 +1,29 @@
+class Solution:
+    """
+    @param nums: The integer array.
+    @param target: Target to find.
+    @return: The first position of target. Position starts from 0.
+    """
+    def binarySearch(self, nums, target):
+        # write your code here
+        if not nums:
+            return -1 
+            
+        start = 0
+        end = len(nums) - 1 
+        
+        while start + 1 < end:
+            mid = start + (end - start) // 2 
+            if nums[mid] > target:
+                end = mid
+            elif nums[mid] < target:
+                start = mid
+            else:
+                end = mid
+        
+        if nums[start] == target:
+            return start
+        if nums[end] == target:
+            return end
+        
+        return -1 
