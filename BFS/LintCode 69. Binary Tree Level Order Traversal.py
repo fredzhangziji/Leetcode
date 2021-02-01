1ab1eb20d2ff0c1cba39cc49441ab33b95befbda
Fred <fredzhangziji@gmail.com>
2021-01-31 20:04:11 -0500

    miner fix

--- BFS/LintCode 69. Binary Tree Level Order Traversal.py
+++ BFS/LintCode 69. Binary Tree Level Order Traversal.py
@@ -1,10 +1,12 @@
 """
 Definition of TreeNode:
+"""
+import collections
 class TreeNode:
     def __init__(self, val):
         self.val = val
         self.left, self.right = None, None
-"""
+
 
 class Solution:
     """
