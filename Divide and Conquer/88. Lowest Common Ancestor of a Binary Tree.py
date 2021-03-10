b923c31b98b3c853add60a73e5968be76684850b
Fred <fredzhangziji@gmail.com>
2021-03-10 18:21:50 -0500

    medium

---
+++ Divide and Conquer/88. Lowest Common Ancestor of a Binary Tree.py
@@ -0,0 +1,38 @@
+"""
+Definition of TreeNode:
+"""
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+
+
+class Solution:
+    """
+    @param: root: The root of the binary search tree.
+    @param: A: A TreeNode in a Binary.
+    @param: B: A TreeNode in a Binary.
+    @return: Return the least common ancestor(LCA) of the two nodes.
+    """
+    def lowestCommonAncestor(self, root, A, B):
+        # write your code here
+        if root is None:
+            return None
+        
+        if root == A or root == B:
+            return root
+        
+        left_result = self.lowestCommonAncestor(root.left, A, B)
+        right_result = self.lowestCommonAncestor(root.right, A, B)
+
+        if left_result and right_result:
+            return root
+
+        if left_result:
+            return left_result
+        
+        if right_result:
+            return right_result
+        
+        return None
