5f9a0c934e937cf24ac5f26751b98558354a8855
Fred <fredzhangziji@gmail.com>
2021-05-29 22:28:13 -0400

    stack ez

---
+++ Data Structure/Lintcode 423 · Valid Parentheses.py
@@ -0,0 +1,24 @@
+class Solution:
+    """
+    @param s: A string
+    @return: whether the string is a valid parentheses
+    """
+    def isValidParentheses(self, s):
+        # write your code here
+        if len(s) % 2 != 0:
+            return False
+
+        stack = []
+        for ch in s:
+            if ch == "(":
+                stack.append(")")
+            elif ch == "[":
+                stack.append("]")
+            elif ch == "{":
+                stack.append("}")
+            
+            else:
+                if not stack or stack.pop() != ch:
+                    return False 
+        
+        return not stack  
