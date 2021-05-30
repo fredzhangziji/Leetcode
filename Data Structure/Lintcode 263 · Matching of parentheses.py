94193564f979471c03cf8fb4127dd8715e154b47
Fred <fredzhangziji@gmail.com>
2021-05-29 22:38:00 -0400

    modified

--- Data Structure/Lintcode 263 · Matching of parentheses.py
+++ Data Structure/Lintcode 263 · Matching of parentheses.py
@@ -18,3 +18,24 @@ class Solution:
                     stack.pop()
             
         return not stack
+
+
+# class Solution:
+#     """
+#     @param string: A string
+#     @return: whether the string is a valid parentheses
+#     """
+#     def matchParentheses(self, string):
+#         # write your code here
+#         num_p = 0
+
+#         for ch in string:
+#             if ch == "(":
+#                 num_p += 1
+#             elif ch == ")":
+#                 num_p -= 1
+            
+#             if num_p < 0:
+#                 return False 
+        
+#         return num_p == 0
