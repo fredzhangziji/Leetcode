bf5d6f417be41d6796c21dc2740aef4b9c170936
fredzhangziji <52152169+fredzhangziji@users.noreply.github.com>
2022-01-11 15:22:21 +0800

    Update 94. Binary Tree Inorder Traversal.c

--- C/94. Binary Tree Inorder Traversal.c
+++ C/94. Binary Tree Inorder Traversal.c
@@ -11,32 +11,50 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int res[100];
-int i = -1; // index for res
 
-void helper(struct TreeNode* root) {
+// 不用全局变量
+void helper(struct TreeNode* root, int* res, int* i) {
     if (root == NULL) {
         return;
     }
-    
-    helper(root->left);
-    i++;
-    res[i] = root->val;
-    helper(root->right);
+    helper(root->left, res, i);
+    res[*i] = root->val;
+    (*i)++;
+    helper(root->right, res, i);
 }
 
 
 int* inorderTraversal(struct TreeNode* root, int* returnSize){
-    // edge case
-    if (root == NULL) {
-        *returnSize = 0;
-        return res;
-    }
-    
-    helper(root);
-    *returnSize = i + 1;    // return size = the final index + 1
-    i = -1;                 // reset the extern variable
+    int *res = (int*)malloc(100 * sizeof(int));
+    *returnSize = 0;
+    helper(root, res, returnSize);
     return res;
-
 }
 
+// 用全局变量
+// int res[100];
+// int i = -1; // index for res
+
+// void helper(struct TreeNode* root) {
+//     if (root == NULL) {
+//         return;
+//     }
+//     helper(root->left);
+//     i++;
+//     res[i] = root->val;
+//     helper(root->right);
+// }
+
+
+// int* inorderTraversal(struct TreeNode* root, int* returnSize){
+//     // edge case
+//     if (root == NULL) {
+//         *returnSize = 0;
+//         return res;
+//     }
+    
+//     helper(root);
+//     *returnSize = i + 1;    // return size = the final index + 1
+//     i = -1;                 // reset the extern variable
+//     return res;
+// }
