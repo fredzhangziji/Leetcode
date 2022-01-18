d3120c89ae8bead7518f15684fa441b0ed954f72
fredzhangziji <52152169+fredzhangziji@users.noreply.github.com>
2022-01-18 20:21:36 +0800

    Create 190. Reverse Bits.c

---
+++ C/190. Reverse Bits.c
@@ -0,0 +1,11 @@
+uint32_t reverseBits(uint32_t n) {
+    uint32_t res = 0;
+    int cnt = 32;
+    while (cnt > 0) {
+        res <<= 1;
+        res += n % 2;
+        n >>= 1;
+        cnt--;
+    }
+    return res;
+}
