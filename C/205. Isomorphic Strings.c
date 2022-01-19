2da4569d8d2e6bccef55020b58ec920ae771c95b
fredzhangziji <52152169+fredzhangziji@users.noreply.github.com>
2022-01-19 20:35:00 +0800

    Update 205. Isomorphic Strings.c

--- C/205. Isomorphic Strings.c
+++ C/205. Isomorphic Strings.c
@@ -16,6 +16,7 @@ bool isIsomorphic(char * s, char * t){
     
     for (int i = 0; i < strlen(s); i++) {
         if (dict[s[i]] == 0) { // first time, map the char to the char in t
+            // check if this t[i] already had a mapping
             for (int j = 0; j < strlen(s); j++) {
                 if (dict[s[j]] == t[i]) {
                     return false;
