da3cbef061c5579c329a140d4755bddddd35dbbb
fredzhangziji <52152169+fredzhangziji@users.noreply.github.com>
2022-01-19 20:05:15 +0800

    Create 205. Isomorphic Strings.c

---
+++ C/205. Isomorphic Strings.c
@@ -0,0 +1,32 @@
+#define SIZE 200
+
+bool isIsomorphic(char * s, char * t){
+    if (strlen(s) != strlen(t)) {
+        return false;
+    }
+    if (strlen(s) == 1) {
+        return true;
+    }
+    
+    // initialize the dict array with 0
+    int dict[SIZE];
+    for (int i = 0; i < SIZE; i++) {
+        dict[i] = 0;
+    }
+    
+    for (int i = 0; i < strlen(s); i++) {
+        if (dict[s[i]] == 0) { // first time, map the char to the char in t
+            for (int j = 0; j < strlen(s); j++) {
+                if (dict[s[j]] == t[i]) {
+                    return false;
+                }
+            }
+            dict[s[i]] = t[i];
+        } else { // already had the mapping
+            if (dict[s[i]] != t[i]) { // the mapping is wrong, return false
+                return false;
+            }
+        }
+    }
+    return true;
+}
